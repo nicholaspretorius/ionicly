003b071ec4f33901b36a2a37559762cc721d1d1f
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-03-11 16:47:57 +0200

    Just made/unmade some changes.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
-
+  onClick() {
+    console.log('Hello world!');
+  }
 }
