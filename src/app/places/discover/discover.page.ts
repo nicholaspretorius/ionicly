6a42ba527511fffd18c5936befd4d967744c7b26
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-03-25 15:39:34 +0200

    Adding places.

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { PlacesService } from './../places.service';
+import { Place } from './../place.model';
 
 @Component({
   selector: 'app-discover',
   templateUrl: './discover.page.html',
-  styleUrls: ['./discover.page.scss'],
+  styleUrls: ['./discover.page.scss']
 })
 export class DiscoverPage implements OnInit {
-
-  constructor() { }
+  places: Place[];
+  constructor(private placesService: PlacesService) {}
 
   ngOnInit() {
+    this.places = this.placesService.places;
   }
 
+  ionViewWillEnter() {}
+
+  navigateTo() {}
 }
--- src/app/places/places.page.ts
+++ src/app/places/places.page.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-places',
   templateUrl: './places.page.html',
-  styleUrls: ['./places.page.scss'],
+  styleUrls: ['./places.page.scss']
 })
 export class PlacesPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
