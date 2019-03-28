15377b8c692c09d40cf5ee7d211be6123264dc7e
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-03-28 11:43:45 +0200

    Adding auth service.

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  userIsAuthenticated = false;
+  constructor() {}
+
+  login() {
+    this.userIsAuthenticated = true;
+  }
+
+  logout() {
+    this.userIsAuthenticated = false;
+  }
+
+  isLoggedIn(): boolean {
+    return this.userIsAuthenticated;
+  }
+}
