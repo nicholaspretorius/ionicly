db94e5248e642aa1879ddf8f6ba578868eef1183
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-03-29 10:20:30 +0200

    Adding bookings.

--- src/app/bookings/bookings.page.ts
+++ src/app/bookings/bookings.page.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { BookingsService } from './bookings.service';
+import { Booking } from './booking.model';
 
 @Component({
   selector: 'app-bookings',
   templateUrl: './bookings.page.html',
-  styleUrls: ['./bookings.page.scss'],
+  styleUrls: ['./bookings.page.scss']
 })
 export class BookingsPage implements OnInit {
+  bookings: Booking[];
 
-  constructor() { }
+  constructor(private bookingsService: BookingsService) {}
 
   ngOnInit() {
+    this.bookings = this.bookingsService.bookings;
   }
-
 }
