67e8c0d4fec706f18af4e2c4672b195f30db93bf
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-03-29 10:10:54 +0200

    Adding swipeable offers.

--- src/app/places/offers/offers.page.ts
+++ src/app/places/offers/offers.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { PlacesService } from '../places.service';
 import { Place } from '../place.model';
+import { IonItemSliding } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-offers',
@@ -10,9 +12,19 @@ import { Place } from '../place.model';
 export class OffersPage implements OnInit {
   offers: Place[];
 
-  constructor(private placesService: PlacesService) {}
+  constructor(private placesService: PlacesService, private router: Router) {}
 
   ngOnInit() {
     this.offers = this.placesService.places;
+  }
+
+  onEdit(offerId: string, slidingOffer: IonItemSliding) {
+    console.log('Editing: ', offerId, slidingOffer);
+    slidingOffer.close();
+    this.router.navigateByUrl(`/places/tabs/offers/${offerId}/edit`);
+  }
+
+  onDelete(offerId: string, slidingOffer: IonItemSliding) {
+    console.log('Deleting: ', offerId, slidingOffer);
   }
 }
