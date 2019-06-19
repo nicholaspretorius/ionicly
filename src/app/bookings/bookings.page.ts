3176b88a907e3ab260bfd70a8f76a34e0468d8b4
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-06-19 14:03:51 +0200

    Added loading indicator to cancel booking.

--- src/app/bookings/bookings.page.ts
+++ src/app/bookings/bookings.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { BookingsService } from './bookings.service';
 import { Booking } from './booking.model';
-import { IonItemSliding } from '@ionic/angular';
+import { IonItemSliding, LoadingController } from '@ionic/angular';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -13,7 +13,7 @@ export class BookingsPage implements OnInit, OnDestroy {
   bookings: Booking[];
   private bookingsSub: Subscription;
 
-  constructor(private bookingsService: BookingsService) {}
+  constructor(private bookingsService: BookingsService, private loadingCtrl: LoadingController) {}
 
   ngOnInit() {
     this.bookingsSub = this.bookingsService.bookings.subscribe(bookings => {
@@ -28,8 +28,13 @@ export class BookingsPage implements OnInit, OnDestroy {
   }
 
   onDelete(bookingId: string, slidingBooking: IonItemSliding) {
-    console.log('Deleting: ', bookingId, slidingBooking);
-    this.bookingsService.cancelBooking(bookingId).subscribe();
-    slidingBooking.close();
+    this.loadingCtrl.create({ message: 'Deleting booking...' }).then(loadingEl => {
+      loadingEl.present();
+      console.log('Deleting: ', bookingId, slidingBooking);
+      this.bookingsService.cancelBooking(bookingId).subscribe(() => {
+        slidingBooking.close();
+        loadingEl.dismiss();
+      });
+    });
   }
 }
