d4f03411925cf469ef451aeadecdf53ccc51ec90
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-06-20 17:15:08 +0200

    Adding persistent bookings.

--- src/app/bookings/bookings.service.ts
+++ src/app/bookings/bookings.service.ts
@@ -3,18 +3,50 @@ import { Booking } from './booking.model';
 import { BehaviorSubject } from 'rxjs';
 import * as uuid from 'uuid/v4';
 import { AuthService } from '../auth/auth.service';
-import { take, tap, delay } from 'rxjs/operators';
+import { take, tap, delay, map, switchMap } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookingsService {
   private _bookings = new BehaviorSubject<Booking[]>([]);
+  private URL = 'https://ionicly-8e283.firebaseio.com/bookings';
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private http: HttpClient) {}
 
   get bookings() {
     return this._bookings.asObservable();
+  }
+
+  fetchBookings() {
+    return this.http.get(this.URL + '.json').pipe(
+      map(bookings => {
+        const newBookings = [];
+        for (const key in bookings) {
+          if (bookings.hasOwnProperty(key)) {
+            newBookings.push(
+              new Booking(
+                key,
+                bookings[key].placeId,
+                bookings[key].userId,
+                bookings[key].placeTitle,
+                bookings[key].placeImage,
+                bookings[key].firstName,
+                bookings[key].lastName,
+                bookings[key].numGuests,
+                bookings[key].dateFrom,
+                bookings[key].dateTo
+              )
+            );
+          }
+        }
+        return newBookings;
+      }),
+      tap(res => {
+        this._bookings.next(res);
+      })
+    );
   }
 
   addBooking(
@@ -42,17 +74,34 @@ export class BookingsService {
       dateTo
     );
 
+    let generatedId: string;
     console.log('Add Booking: ', newBooking);
 
-    // add new booking to observable
-    return this._bookings.pipe(
-      take(1),
-      delay(1000),
-      tap(bookings => {
-        this._bookings.next(bookings.concat(newBooking));
-        console.log('Bookings: ', this._bookings);
+    return this.http
+      .post<{ name: string }>(this.URL + '.json', {
+        ...newBooking,
+        id: null
       })
-    );
+      .pipe(
+        switchMap(res => {
+          console.log('Bookings: ', res);
+          return this.bookings;
+        }),
+        take(1),
+        tap(bookings => {
+          newBooking.id = generatedId;
+          this._bookings.next(bookings.concat(newBooking));
+        })
+      );
+    // add new booking to observable
+    // return this._bookings.pipe(
+    //   take(1),
+    //   delay(1000),
+    //   tap(bookings => {
+    //     this._bookings.next(bookings.concat(newBooking));
+    //     console.log('Bookings: ', this._bookings);
+    //   })
+    // );
   }
 
   cancelBooking(bookingId: string) {
